94eb4f10c439f31425f8fed66ca455bffba2d50e
Salva <salvachll@gmail.com>
2021-10-02 12:14:37 +0200

    Datos obtenidos

--- cypress/integration/camera.spec.js
+++ cypress/integration/camera.spec.js
@@ -6,88 +6,94 @@ describe('visitamos pccomponentes para obtener datos de smartphones', () => {
   });
 
   it('Recorremos los productos', () => {
-    cy.get('.zg-item').each((item, index) => {
-      const product = {}
-      cy.get('.zg-item').eq(index).click();
-      cookies();
+    cy.get('.zg-item').then(items => {
+      items.toArray().slice(14, 49).forEach((item, index) => {
 
-      cy.get('body').then(body => {
-        if (body.find('#productTitle').length) {
-          cy.get('#productTitle').then(title => {
-            product.title = title.text().trim();
-          });
-        }
-      })
+        index = index + 14;
+        const product = {}
+        cy.get('.zg-item').eq(index).click();
+        cookies();
 
-      cy.get('body').then(body => {
-        if (body.find('#newBuyBoxPrice').length) {
-          cy.get('#newBuyBoxPrice').then(price => {
-            product.price = price.text().trim();
-          })
-        } else if (body.find('#price_inside_buybox').length) {
-          cy.get('#price_inside_buybox').then(price => {
-            product.price = price.text().trim();
-          })
-        }
-      })
-
-      cy.get('body').then(body => {
-        if (body.find('.imageThumbnail').length) {
-          cy.get('.imageThumbnail').each(thumb => {
-            cy.get('img.a-dynamic-image').eq(0).then(img => {
-              if (product.image) {
-                product.image.push(img.prop('src'));
-              } else {
-                product.image = [img.prop('src')]
-              }
+        cy.get('body').then(body => {
+          if (body.find('#productTitle').length) {
+            cy.get('#productTitle').then(title => {
+              product.title = title.text().trim();
             });
-          })
-        }
-      })
+          }
+        })
 
-      cy.get('body').then(body => {
-        if (body.find('#detailBullets_feature_div > .a-unordered-list li').length) {
-          cy.get('#detailBullets_feature_div > .a-unordered-list li').each(prop => {
-            cy.wrap(prop).within(() => {
-              cy.get('.a-text-bold').then(caract => {
-                cy.get('.a-list-item > :nth-child(2)').then(desc => {
-                  const full = {}
-                  full[caract.text().trim().trim().replace('\n', '')] = desc.text().trim();
-                  if (product.feature) {
+        cy.get('body').then(body => {
+          if (body.find('#newBuyBoxPrice').length) {
+            cy.get('#newBuyBoxPrice').then(price => {
+              product.price = price.text().trim();
+            })
+          } else if (body.find('#price_inside_buybox').length) {
+            cy.get('#price_inside_buybox').then(price => {
+              product.price = price.text().trim();
+            })
+          }
+        })
 
-                    product.feature.push(full);
+        cy.get('body').then(body => {
+          if (body.find('.imageThumbnail').length) {
+            cy.get('.imageThumbnail').each(thumb => {
+              cy.wrap(thumb).trigger('mouseenter').trigger('mouseover').click({ force: true });
+              cy.wait(2000)
+              cy.get('.image.item.selected img.a-dynamic-image').eq(0).then(img => {
+                if (product.image) {
+                  product.image.push(img.prop('src'));
+                } else {
+                  product.image = [img.prop('src')]
+                }
+              });
+            })
+          }
+        })
+
+        cy.get('body').then(body => {
+          if (body.find('#detailBullets_feature_div > .a-unordered-list li').length) {
+            cy.get('#detailBullets_feature_div > .a-unordered-list li').each(prop => {
+              cy.wrap(prop).within(() => {
+                cy.get('.a-text-bold').then(caract => {
+                  cy.get('.a-list-item > :nth-child(2)').then(desc => {
+                    const full = {}
+                    full[caract.text().trim().trim().replace('\n', '')] = desc.text().trim();
+                    if (product.feature) {
+
+                      product.feature.push(full);
+                    } else {
+                      product.feature = [full]
+                    }
+                  })
+                })
+              })
+            })
+          }
+        })
+
+        cy.get('body').then(body => {
+          if (body.find('#feature-bullets > .a-unordered-list > li').length) {
+
+            cy.get('#feature-bullets > .a-unordered-list > li').each(description => {
+              cy.wrap(description).within(() => {
+                cy.get('.a-list-item').then(desc => {
+                  if (product.description) {
+                    product.description.push(desc.text().trim())
                   } else {
-                    product.feature = [full]
+                    product.description = [desc.text().trim()]
                   }
                 })
               })
             })
-          })
-        }
-      })
+          }
+        })
 
-      cy.get('body').then(body => {
-        if (body.find('#feature-bullets > .a-unordered-list > li').length) {
-
-          cy.get('#feature-bullets > .a-unordered-list > li').each(description => {
-            cy.wrap(description).within(() => {
-              cy.get('.a-list-item').then(desc => {
-                if (product.description) {
-                  product.description.push(desc.text().trim())
-                } else {
-                  product.description = [desc.text().trim()]
-                }
-              })
-            })
-          })
-        }
+        cy.get('body').then(() => {
+          console.log(product)
+          cy.writeFile(`./camera/${product.title.substring(0, 25).replace('/', '').trim()}.json`, JSON.stringify(product))
+        })
+        visit();
       })
-
-      cy.get('body').then(() => {
-        console.log(product)
-        cy.writeFile(`./camera/${product.title.substring(0, 25).replace('/', '').trim()}.json`, JSON.stringify(product))
-      })
-      visit();
     })
   })
 })
--- cypress/integration/tv.spec.js
+++ cypress/integration/tv.spec.js
@@ -6,94 +6,100 @@ describe('visitamos pccomponentes para obtener datos de smartphones', () => {
   });
 
   it('Recorremos los productos', () => {
-    cy.get('.zg-item').each((item, index) => {
-      const product = {}
-      cy.get('.zg-item').eq(index).click();
-      cookies();
+    cy.get('.zg-item').then(items => {
+      items.toArray().slice(15, 49).forEach((item, index) => {
 
-      cy.get('body').then(body => {
-        if (body.find('#productTitle').length) {
-          cy.get('#productTitle').then(title => {
-            product.title = title.text().trim();
-          });
-        }
-      })
+        index = index + 15;
+        const product = {}
+        cy.get('.zg-item').eq(index).click();
+        cookies();
 
-      cy.get('body').then(body => {
-        if (body.find('#newBuyBoxPrice').length) {
-          cy.get('#newBuyBoxPrice').then(price => {
-            product.price = price.text().trim();
-          })
-        } else if (body.find('#price_inside_buybox').length) {
-          cy.get('#price_inside_buybox').then(price => {
-            product.price = price.text().trim();
-          })
-        }
-      })
-
-      cy.get('body').then(body => {
-        if (body.find('.imageThumbnail').length) {
-          cy.get('.imageThumbnail').each(thumb => {
-            cy.get('img.a-dynamic-image').eq(0).then(img => {
-              if (product.image) {
-                product.image.push(img.prop('src'));
-              } else {
-                product.image = [img.prop('src')]
-              }
+        cy.get('body').then(body => {
+          if (body.find('#productTitle').length) {
+            cy.get('#productTitle').then(title => {
+              product.title = title.text().trim();
             });
-          })
-        }
-      })
+          }
+        })
 
-      cy.get('body').then(body => {
-        if (body.find('#detailBullets_feature_div > .a-unordered-list li').length) {
-          cy.get('#detailBullets_feature_div > .a-unordered-list li').each(prop => {
-            cy.wrap(prop).within(() => {
-              cy.get('.a-text-bold').then(caract => {
-                cy.get('.a-list-item > :nth-child(2)').then(desc => {
-                  const full = {}
-                  full[caract.text().trim().trim().replace('\n', '')] = desc.text().trim();
-                  if (product.feature) {
+        cy.get('body').then(body => {
+          if (body.find('#newBuyBoxPrice').length) {
+            cy.get('#newBuyBoxPrice').then(price => {
+              product.price = price.text().trim();
+            })
+          } else if (body.find('#price_inside_buybox').length) {
+            cy.get('#price_inside_buybox').then(price => {
+              product.price = price.text().trim();
+            })
+          }
+        })
 
-                    product.feature.push(full);
+        cy.get('body').then(body => {
+          if (body.find('.imageThumbnail').length) {
+            cy.get('.imageThumbnail').each(thumb => {
+              cy.wrap(thumb).trigger('mouseenter').trigger('mouseover').click({ force: true });
+              cy.wait(2000)
+              cy.get('.image.item.selected img.a-dynamic-image').eq(0).then(img => {
+                if (product.image) {
+                  product.image.push(img.prop('src'));
+                } else {
+                  product.image = [img.prop('src')]
+                }
+              });
+            })
+          }
+        })
+
+        cy.get('body').then(body => {
+          if (body.find('#detailBullets_feature_div > .a-unordered-list li').length) {
+            cy.get('#detailBullets_feature_div > .a-unordered-list li').each(prop => {
+              cy.wrap(prop).within(() => {
+                cy.get('.a-text-bold').then(caract => {
+                  cy.get('.a-list-item > :nth-child(2)').then(desc => {
+                    const full = {}
+                    full[caract.text().trim().trim().replace('\n', '')] = desc.text().trim();
+                    if (product.feature) {
+
+                      product.feature.push(full);
+                    } else {
+                      product.feature = [full]
+                    }
+                  })
+                })
+              })
+            })
+          }
+        })
+
+        cy.get('body').then(body => {
+          if (body.find('#feature-bullets > .a-unordered-list > li').length) {
+
+            cy.get('#feature-bullets > .a-unordered-list > li').each(description => {
+              cy.wrap(description).within(() => {
+                cy.get('.a-list-item').then(desc => {
+                  if (product.description) {
+                    product.description.push(desc.text().trim())
                   } else {
-                    product.feature = [full]
+                    product.description = [desc.text().trim()]
                   }
                 })
               })
             })
-          })
-        }
-      })
+          }
+        })
 
-      cy.get('body').then(body => {
-        if (body.find('#feature-bullets > .a-unordered-list > li').length) {
-
-          cy.get('#feature-bullets > .a-unordered-list > li').each(description => {
-            cy.wrap(description).within(() => {
-              cy.get('.a-list-item').then(desc => {
-                if (product.description) {
-                  product.description.push(desc.text().trim())
-                } else {
-                  product.description = [desc.text().trim()]
-                }
-              })
-            })
-          })
-        }
+        cy.get('body').then(() => {
+          console.log(product)
+          cy.writeFile(`./tv/${product.title.substring(0, 25).replace('/', '').trim()}.json`, JSON.stringify(product))
+        })
+        visit();
       })
-
-      cy.get('body').then(() => {
-        console.log(product)
-        cy.writeFile(`./tv/${product.title.substring(0, 25).replace('/', '').trim()}.json`, JSON.stringify(product))
-      })
-      visit();
     })
   })
 })
 
 function visit() {
-  cy.visit('https://www.amazon.es/gp/bestsellers/electronics/664659031?ref_=Oct_d_obs_S&pd_rd_w=ejze3&pf_rd_p=62dfbf75-5eb5-4489-a0aa-2d40ffc9f548&pf_rd_r=W0F28W3DMY0JJPY6VGXK&pd_rd_r=00640c95-9385-4c0e-ae0b-b32ded6168d5&pd_rd_wg=IwNAM');
+  cy.visit('https://www.amazon.es/gp/bestsellers/electronics/934359031/ref=zg_bs_nav_e_2_664659031');
   cy.get('body').then(body => {
     if (body.find('.a-button-input').length) {
       cy.get('.a-button-input').click();
